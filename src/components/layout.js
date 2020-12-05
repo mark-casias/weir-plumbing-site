51ef8a280a44698ac64d0a399c2f699444377ca8
Mark Casias <markie@kanopistudios.om>
2020-12-05 13:40:09 -0700

    metadata fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+  /* const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -21,7 +20,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `) */
 
   return (
     <>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -7,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Weir Plumbing, Heating and Cooling" />
     <div>
       <Image />
     </div>
